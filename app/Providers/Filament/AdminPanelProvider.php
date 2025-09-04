1483015fa2af32d937365f7cfdc3e32030fdc559
Wasanjith Kodikara <149646160+wasanjith@users.noreply.github.com>
2025-09-04 16:11:07 +0530

    remove scaffold manager from the dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -79,6 +79,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])->plugin(FilamentScaffoldPlugin::make());
+            ]);
     }
 }
